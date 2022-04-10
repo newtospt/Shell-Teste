ccf2a2930551920432ed0f177289090bfbaafc18
Newton Neto <newton.neto@ibm.com>
2022-04-10 09:11:11 -0300

    Adicionando primeiro script

---
+++ PrimeiroScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DATAHORA=`date +%H:%M`
+
+clear
+echo "==== Meu Primeiro Script ==="
+echo ""
+echo -n "Exibir data e hora atual: $DATAHORA "
+echo "==========================="
+echo "Listagem dos Alunos: "
+cd ../Sport Club
+ls -lrt
